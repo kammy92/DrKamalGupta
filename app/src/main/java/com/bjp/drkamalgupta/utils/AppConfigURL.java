20ad348929d6dca217722260ff932eaefe47b4ad
kammy92 <karman.singhh@gmail.com>
2019-03-08 12:51:08 +0530

    Updated URLs

--- app/src/main/java/com/bjp/drkamalgupta/utils/AppConfigURL.java
+++ app/src/main/java/com/bjp/drkamalgupta/utils/AppConfigURL.java
@@ -1,11 +1,10 @@
 package com.bjp.drkamalgupta.utils;
 
 public class AppConfigURL {
-    //    public static String version = "v1.4";
     public static String version = "v1.0";
     
-    //    private static String BASE_URL = "http://drkamalguptabjp.com/api/" + version + "/";
-    private static String BASE_URL = " https://factory-app-cammy92.c9users.io/api_temp/" + version + "/";
+    private static String BASE_URL = "http://drkamalguptabjp.com/api/" + version + "/";
+//    private static String BASE_URL = " https://factory-app-cammy92.c9users.io/api_temp/" + version + "/";
 
     public static String URL_INIT = BASE_URL + "app/init/application";
 }
